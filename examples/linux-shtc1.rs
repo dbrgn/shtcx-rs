c69017468dd9a7787f7518124e99270ba29ca777
Danilo Bargen <mail@dbrgn.ch>
2024-05-10 19:32:48 +0200

    Examples: Replace unwrap with expect
    ...and some more useful error messages.

--- examples/linux-shtc1.rs
+++ examples/linux-shtc1.rs
@@ -12,21 +12,25 @@ fn main() {
     println!();
 
     println!("Soft reset...");
-    sht.reset(&mut delay).unwrap();
+    sht.reset(&mut delay).expect("Soft reset failed");
     println!();
 
     println!(
         "Device identifier: 0x{:02x}",
-        sht.device_identifier().unwrap()
+        sht.device_identifier()
+            .expect("Failed to get device identifier")
     );
     println!(
         "Raw ID register:   0b{:016b}",
-        sht.raw_id_register().unwrap()
+        sht.raw_id_register()
+            .expect("Failed to get raw ID register")
     );
 
     println!("\nNormal mode measurements:");
     for _ in 0..3 {
-        let measurement = sht.measure(PowerMode::NormalMode, &mut delay).unwrap();
+        let measurement = sht
+            .measure(PowerMode::NormalMode, &mut delay)
+            .expect("Normal mode measurement failed");
         println!(
             "  {:.2} °C | {:.2} %RH",
             measurement.temperature.as_degrees_celsius(),
@@ -36,7 +40,9 @@ fn main() {
 
     println!("\nLow power mode measurements:");
     for _ in 0..3 {
-        let measurement = sht.measure(PowerMode::LowPower, &mut delay).unwrap();
+        let measurement = sht
+            .measure(PowerMode::LowPower, &mut delay)
+            .expect("Low power mode measurement failed");
         println!(
             "  {:.2} °C | {:.2} %RH",
             measurement.temperature.as_degrees_celsius(),
